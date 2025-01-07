abe363fd7444d60747e93fa76a935848251ff5f6
kmkyra <kennedykyra69@gmail.com>
2025-01-07 21:28:53 +0000

    done

--- classwork.cpp
+++ classwork.cpp
@@ -7,19 +7,31 @@ int main()
     int num1;
     std::string noun1;
     std::string adjective1;
+    std::string name1;
+    std::string noun2;
+    std::string name2;
+    std::string verb1;
 
     // Get user input
-    std::cout << "Enter a number: ";
+    std::cout << "Enter a number: \n";
     std::cin >> num1;
 
-    std::cout << "Enter a noun: ";
-    std::cin >> noun1;
+    std::cout << "Enter two nouns: \n";
+    std::cin >> noun1 >> noun2;
 
-    std::cout << "Enter an adjective: ";
+    std::cout << "Enter an adjective: \n";
     std::cin >> adjective1;
 
+    std::cout << "Enter two names: \n";
+    std::cin >> name1 >> name2;
+
+    std::cout << "Enter a verb: \n";
+    std::cin >> verb1;
+
     // Display story
-    std::cout << "There was once a " << noun1 << " named Larry. They were very " << adjective1 << ", and they were " << num1 << " years old.\n";
+    std::cout << "There was once a " << noun1 << " named " << name1 << ". They were very " << adjective1 << ", and they were " 
+    << num1 << " years old.\n " "One day, " << name1 << " met a " << noun2 << " named " << name2 << ". Together, they "
+    << verb1 << " and live happily ever after.";
     
     return 0;
 }
